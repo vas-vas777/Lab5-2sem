9ed584c645c80e38961955dde27696918ec074ff
vas-vas777 <vasya1998@list.ru>
2017-04-16 20:06:28 +0300

    Create main.cpp

---
+++ example/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <stdlib.h>
+
+#include "Lab5-2sem.hpp"
+using namespace std;
+
+int main()
+{ 
+        BinarySearchTree<unsigned int> *root = 0; 
+        return 0;
+}
