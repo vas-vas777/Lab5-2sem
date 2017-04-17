34984c58d6c3779dc3b33d00e7d8b8d099774fe5
vas-vas777 <vasya1998@list.ru>
2017-04-17 17:42:42 +0300

    Update init.cpp

--- tests/source/init.cpp
+++ tests/source/init.cpp
@@ -2,36 +2,36 @@
 #include <catch.hpp>
 SCENARIO("add") 
 {    
-BinarySearchTree<unsigned int> r;    
-BinarySearchTree<unsigned int>* root;    
+BinarySearchTree<int> r;    
+BinarySearchTree<int>* root;    
 int rv = r.add(10, root);        
 REQUIRE( rv == 10);
 }
 SCENARIO("print") 
 {    
-BinarySearchTree<unsigned int> r;    
-BinarySearchTree<unsigned int>* root;    
+BinarySearchTree<int> r;    
+BinarySearchTree<int>* root;    
 int rv = r.print(11, root);        
 REQUIRE( rv == 11);
 }
 SCENARIO("search") 
 {    
-BinarySearchTree<unsigned int> r;    
-BinarySearchTree<unsigned int>* root;    
+BinarySearchTree<int> r;    
+BinarySearchTree<int>* root;    
 int rv = r.search(12, root);        
 REQUIRE( rv == 12);
 }
 SCENARIO("del") 
 {    
-BinarySearchTree<unsigned int> r;    
-BinarySearchTree<unsigned int>* root;    
+BinarySearchTree<int> r;    
+BinarySearchTree<int>* root;    
 int rv = r.search(13, root);        
 REQUIRE( rv == 13);
 }
 SCENARIO("delTree") 
 {    
-BinarySearchTree<unsigned int> r;    
-BinarySearchTree<unsigned int>* root;    
+BinarySearchTree<int> r;    
+BinarySearchTree<int>* root;    
 int rv = r.search(14, root);        
 REQUIRE( rv == 14);
 }
