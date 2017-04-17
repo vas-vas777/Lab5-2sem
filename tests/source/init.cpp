822f20fe2b6d06d5906c838e6025a8349eebeb62
vas-vas777 <vasya1998@list.ru>
2017-04-17 17:47:26 +0300

    Update init.cpp

--- tests/source/init.cpp
+++ tests/source/init.cpp
@@ -1,37 +1,37 @@
 #include <Lab5-2sem.hpp>
 #include <catch.hpp>
-SCENARIO("add") 
-{    
+
+SCENARIO("add") {    
 BinarySearchTree<int> r;    
-BinarySearchTree<int>* root;    
+BinarySearchTree<int> *root;    
 int rv = r.add(10, root);        
 REQUIRE( rv == 10);
 }
 SCENARIO("print") 
 {    
 BinarySearchTree<int> r;    
-BinarySearchTree<int>* root;    
+BinarySearchTree<int> *root;    
 int rv = r.print(11, root);        
 REQUIRE( rv == 11);
 }
 SCENARIO("search") 
 {    
 BinarySearchTree<int> r;    
-BinarySearchTree<int>* root;    
+BinarySearchTree<int> *root;    
 int rv = r.search(12, root);        
 REQUIRE( rv == 12);
 }
 SCENARIO("del") 
 {    
 BinarySearchTree<int> r;    
-BinarySearchTree<int>* root;    
+BinarySearchTree<int> *root;    
 int rv = r.search(13, root);        
 REQUIRE( rv == 13);
 }
 SCENARIO("delTree") 
 {    
 BinarySearchTree<int> r;    
-BinarySearchTree<int>* root;    
+BinarySearchTree<int> *root;    
 int rv = r.search(14, root);        
 REQUIRE( rv == 14);
 }
